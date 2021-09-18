8b26dfc63680d8c09a981a679293cd9019e4696b
GemaSP <73533582+GemaSP@users.noreply.github.com>
2021-09-18 12:33:46 +0700

    Update Contoh1.php

--- book/application/controllers/Contoh1.php
+++ book/application/controllers/Contoh1.php
@@ -4,6 +4,7 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Muhamad Hoerudin, saya tinggal di Bogor dan saya suka bersepeda";
+        echo "<br>Nama saya Muhamad Hoerudin, saya tinggal di Bogor dan saya suka bersepeda"</br>;
+        echo "saya sangat tampan"
     }
 }
